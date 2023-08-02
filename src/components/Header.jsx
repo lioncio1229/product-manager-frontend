07480f1cb814be63bd4ebc3a08033950f16010ff
Lioncio Morcilla <lionciomorcilla12@gmail.com>
2023-08-02 11:05:05 +0800

    Add User Account Menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,48 @@
-import {Box, Container, Stack, Typography, Tooltip, IconButton, Avatar } from "@mui/material";
+import { useState } from "react";
+import {Box, Container, Stack, Typography, Tooltip, IconButton, Avatar, Menu, MenuItem, ListItemText, ListItemIcon } from "@mui/material";
 import { Person } from "@mui/icons-material";
+import { Logout } from "@mui/icons-material";
 
-function Header(){
+function Header({userName='username@email.com'}){
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+
     return <>
         <Box bgcolor='primary.main' p={1} boxShadow={2}>
             <Container maxWidth='lg'>
                 <Stack direction='row' justifyContent='space-between' alignItems='center'>
                     <Typography color="white" variant="h5" component="a" href="/" sx={{textDecoration: 'none'}}>Product Manager</Typography>
-                    <Tooltip title="Open settings">
-                        <IconButton sx={{ p: 0 }}>
+                    <Tooltip title="Open Account">
+                        <IconButton sx={{ p: 0 }} onClick={handleClick}>
                             <Avatar> <Person/> </Avatar>
                         </IconButton>
                     </Tooltip>
+                    <Menu
+                        id="basic-menu"
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+                        MenuListProps={{
+                        'aria-labelledby': 'basic-button',
+                        }}
+                    >
+                    <MenuItem>
+                        <ListItemText>{userName}</ListItemText>
+                    </MenuItem>
+                    <MenuItem onClick={handleClose}>
+                        <ListItemIcon>
+                            <Logout color='primary' fontSize="small" />
+                        </ListItemIcon>
+                        <ListItemText>Logout</ListItemText>
+                    </MenuItem>
+                </Menu>
                 </Stack>
             </Container>
         </Box>
